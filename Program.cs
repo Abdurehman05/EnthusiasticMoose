5b5466e6a573f5d13f991e7f816f5cb3dde4b1e7
Abdu <abdukr5@gmail.com.com>
2020-11-12 23:12:00 -0800

    print welcome string

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EnthusiasticMoose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+            Console.WriteLine("--------------------------------------------");
+
+        }
+    }
+}
